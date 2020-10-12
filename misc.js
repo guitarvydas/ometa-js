71ec872b676883e81940346199a759d133a2efbb
Paul Tarvydas <guitarvydas@gmail.com>
2020-10-11 22:31:23 -0400

    added misc.js - miscellaneous helper functions for tutorial*.html

---
+++ misc.js
@@ -0,0 +1,15 @@
+function displayDate () {
+    document.getElementById('timestamp').innerHTML = Date();
+}
+
+function displayTree (s) {
+    document.getElementById('tree').innerHTML = s;
+}
+
+function displayCompiled (s) {
+    document.getElementById('compiled').innerHTML = s;
+}
+
+function displayResult (s) {
+    document.getElementById('result').innerHTML = s;
+}
